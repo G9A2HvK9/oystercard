c78484fb82b6e5fe3448ef280605ea0c913e62e7
hendrik <g9a2hvk9@me.com>
2017-04-19 18:36:02 +0100

    Adds Journey and Station class, as well as _spec files. Adds Penalty to parameters

--- lib/parameters.rb
+++ lib/parameters.rb
@@ -1,4 +1,4 @@
 MAX_BALANCE = 90 # Maximum Oystercard balance
 MIN_BALANCE = 1 # Minimum balance for a single journey
-MIN_FARE = 1
-PENALTY = 6
+MIN_FARE = 1 # Minimum fare to be deducted per journey
+PENALTY = 6 # Penalty for travelling without touching_in or touching_out
