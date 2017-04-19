4fc46ae786747e744f37f4cd0710e700116ee8d9
hendrik <g9a2hvk9@me.com>
2017-04-19 15:54:15 +0100

    Complete #11 = Adds journey beginning and end functionlity. Refactors to remove @in_transit variable. Updates #in_transit method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,10 @@
 require_relative "parameters"
 
 class Oystercard
-attr_reader :balance, :in_transit
+attr_reader :balance, :journey_start, :journey_end # <-- CHANGE!
 
   def initialize(balance = 0)
     @balance = balance
-    @in_transit = false
   end
 
   def top_up(amount = 0)
@@ -17,15 +16,21 @@ attr_reader :balance, :in_transit
     @balance -= amount
   end
 
-  def touch_in
-    raise "You are already in transit; please touch out before beginning a new journey." if @in_transit == true
+  def touch_in(journey_start = nil)
+    raise "You are already in transit; please touch out before beginning a new journey." if in_transit?
     raise "You don't have sufficient funds. Please top up your card." if @balance < MIN_BALANCE
-    @in_transit = true
+    @journey_end = nil
+    @journey_start = journey_start
   end
 
-  def touch_out
-    deduct(MIN_FARE) unless @in_transit == false
-    @in_transit = false
+  def touch_out(journey_end = nil)
+    deduct(MIN_FARE) if in_transit?
+    @journey_end = journey_end
+    @journey_start = nil
+  end
+
+  def in_transit?
+    (!@journey_start.nil? && @journey_end.nil?) ? true : false
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,8 +6,8 @@ describe Oystercard do
     it "should be an instance of the Oystercard class" do
       expect(subject).to be_a Oystercard
     end
-    it "should have two attributes known as :balance and :in_transit" do
-      expect(Oystercard.new).to have_attributes(:balance => 0, :in_transit => false)
+    it "should have an attribute known as :balance" do
+      expect(Oystercard.new).to have_attributes(:balance => 0)
     end
     context "when balance attribute is left as the default value" do
       it "should have a balance of 0" do
@@ -61,17 +61,11 @@ describe Oystercard do
   end
 
   describe ".touch_in" do
-    it { is_expected.to respond_to(:touch_in) }
-    context "when @in_transit is equal to false" do
-      it "changes the value of @in_transit from false to true" do
-        subject.top_up(::MIN_BALANCE)
-        expect{ subject.touch_in }.to change{ subject.in_transit }.from(false).to(true)
-      end
-    end
-    context "when @in_transit is equal to true" do
+    it { is_expected.to respond_to(:touch_in).with(1).argument }
+    context "when already in_transit" do
       it "raises an error message" do
         subject.top_up(::MIN_BALANCE)
-        subject.touch_in
+        subject.touch_in("East") # in_transit will be true
         expect{ subject.touch_in }.to raise_error("You are already in transit; please touch out before beginning a new journey.")
       end
     end
@@ -81,28 +75,49 @@ describe Oystercard do
         expect{ subject.touch_in }.to raise_error "You don't have sufficient funds. Please top up your card."
       end
     end
+    context "when journey_start is equal to default" do
+      it "it should be equal to nil" do
+        subject.top_up(::MIN_BALANCE + ::MIN_FARE)
+        subject.touch_in
+        expect( subject.journey_start ).to eq nil
+      end
+    end
+    context "when touching_in at Aldgate" do
+      it "let's journey_start equal Aldgate" do
+        subject.top_up(::MIN_BALANCE + ::MIN_FARE)
+        subject.touch_in("Aldgate")
+        expect( subject.journey_start ).to eq("Aldgate")
+      end
+    end
   end
 
   describe ".touch_out" do
-    it { is_expected.to respond_to(:touch_out) }
-    context "when @in_transit is equal to true" do
-      it "changes the value of @in_transit from true to false" do
-        subject.top_up(::MIN_BALANCE)
-        subject.touch_in
-        expect{subject.touch_out}.to change{ subject.in_transit }.from(true).to(false)
-      end
-    end
-    context "when @in_transit is equal to true" do
+    it { is_expected.to respond_to(:touch_out).with(1).argument }
+    context "when in_transit" do
       it "decrease the @balance on the card by #{MIN_FARE}" do
         subject.top_up(::MIN_FARE + ::MIN_BALANCE)
-        subject.touch_in
+        subject.touch_in("Aldgate") # indicating in_transit is true
         expect{ subject.touch_out }.to change{ subject.balance }.by(-::MIN_FARE)
       end
     end
-    context "when @in_transit is equal to false" do
+    context "when in_transit is false" do
       it "should not change the value of balance" do
         subject.top_up(::MIN_FARE + ::MIN_BALANCE)
+        subject.touch_in("East")
+        subject.touch_out("Aldgate")
         expect{ subject.touch_out }.not_to change{ subject.balance }
+      end
+    end
+    context "when journey_end is equal to default" do
+      it "it should be equal to nil" do
+        subject.touch_out
+        expect( subject.journey_end ).to eq nil
+      end
+    end
+    context "when touching_out at Aldgate" do
+      it "let's journey_end equal Aldgate" do
+        subject.touch_out("Aldgate")
+        expect( subject.journey_end ).to eq("Aldgate")
       end
     end
   end
